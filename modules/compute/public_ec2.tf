2eb5f0afee1945f7ca63dfbce7acc34f3ff5958a
suryagit12 <surya.anali@gmail.com>
2025-02-24 14:00:49 +0530

    typo corrected

--- modules/compute/public_ec2.tf
+++ modules/compute/public_ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "public-servers" {
   key_name                    = var.key_name
   iam_instance_profile = var.iam_instance_profile
   subnet_id                   = element(var.public_subnets, count.index)
-  vpc_security_group_ids      = var.sg_id
+  vpc_security_group_ids      = [var.sg_id]
   associate_public_ip_address = true
   tags = {
     Name        = "${var.vpc_name}-Public-Server-${count.index + 1}"

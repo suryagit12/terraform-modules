73120a016adb2f12e13bceb341340f7b97eda0e9
suryagit12 <98840080+suryagit12@users.noreply.github.com>
2025-02-24 10:57:08 +0000

    provider added

--- s3.tf
+++ s3.tf
@@ -1,8 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+
+}
+
 resource "aws_s3_bucket" "test" {
-    bucket  = "test-surya-terra"
-    tags = {
-      Name = "test"
-      Env = "dev"
-    }
-  
+  bucket = "test-surya-terra"
+  tags = {
+    Name = "test"
+    Env  = "dev"
+  }
+
 }

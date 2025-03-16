7674e891ab7210066ac4fee0cf9cb9132565e444
suryagit12 <surya.anali@gmail.com>
2025-03-16 12:47:04 +0530

    path adjusted and arn default updated

--- modules/lamada/variables.tf
+++ modules/lamada/variables.tf
@@ -7,7 +7,11 @@ variable "lambda_exec_policy_name" {}
 variable "environment" { }
 variable "subnet_ids" { default = [] }
 variable "security_group_ids" { }
-variable "s3_bucket_name" {}
+variable "s3_bucket_arn" {
+  description = "ARN of the S3 bucket"
+  type        = string
+  default     = "arn:aws:s3:::lambda-bucket/*"
+}
 variable "s3_object_key" {}
-variable "s3_bucket_arn" {}
+variable "s3_bucket_name" {}
 variable "vpc_name" {}

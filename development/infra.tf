c81ad5540bd8f70e159d3c74f9d46cd63baab474
suryagit12 <surya.anali@gmail.com>
2025-03-26 22:05:34 +0530

    re

--- development/infra.tf
+++ development/infra.tf
@@ -9,8 +9,8 @@ module "dev_vpc_1" {
   natgw_id           = module.dev_natgw_1.natgw_id
 }
 
-# module "dev_natgw_1" {
-#   source           = "../modules/nat"
-#   public_subnet_id = module.dev_vpc_1.public_subnets_id_1
-#   vpc_name         = module.dev_vpc_1.vpc_name
-# }
+module "dev_natgw_1" {
+  source           = "../modules/nat"
+  public_subnet_id = module.dev_vpc_1.public_subnets_id_1
+  vpc_name         = module.dev_vpc_1.vpc_name
+}

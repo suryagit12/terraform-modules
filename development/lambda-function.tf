ca20b2888f27bb4e29f8b6fce4fdda7942dd9c45
suryagit12 <surya.anali@gmail.com>
2025-03-26 16:55:43 +0530

    lambda typo corrected

--- development/lambda-function.tf
+++ development/lambda-function.tf
@@ -1,15 +1,15 @@
 module "lambda-function" {
-   source      = "../modules/lamada"
-   lambda_function_name = "lamada-dev-function"
+   source      = "../modules/lambda"
+   lambda_function_name = "lambda-dev-function"
    lambda_handler = "lambda_function.lambda_handler"
    lambda_runtime = "python3.8"
-   lambda_zip_path = "../development/lamada_function.py"
+   lambda_zip_path = "../development/lambda_function.py"
    lambda_exec_role_name = "lambda_exec_role"
    lambda_exec_policy_name = "lambda_exec_policy"
    environment  = module.dev_vpc_1.environment
    subnet_ids  = module.dev_vpc_1.private_subnets_id
    security_group_ids = [module.dev_sg_1.sg_id]
-   s3_bucket_name = "lamada-bucket"
+   s3_bucket_name = "lambda-bucket"
    s3_object_key = "lambda_function.py"
    s3_bucket_arn = "s3_bucket_arn"
    vpc_name     = module.dev_vpc_1.vpc_name
